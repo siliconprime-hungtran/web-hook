9d7612de08bfbb6c1ea9ecea22c177ccd9a3bd36
Hung Tran <hungtran@siliconprime.us>
2016-06-26 18:33:53 +0700

    .

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.post('/webhook', function(req, res) {
+
+});
+
+
 module.exports = router;
