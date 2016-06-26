1e66061bdd2de344975bda679217f89c06fd4b58
Hung Tran <hungtran@siliconprime.us>
2016-06-26 18:24:30 +0700

    .

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var webhook = require('./routes/webhook');
 
 var app = express();
 
@@ -24,11 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
-ost('/webhook', function(req, res) {
-  //  We'll add this next
-});
-
+app.use('/webhook', webhook);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
